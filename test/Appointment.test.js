3642882532dd59ad7514e2360ad1dfaf65af7c6d
golden05 <golden05@gmail.com>
2022-11-23 22:13:53 +0800

    init

---
+++ test/Appointment.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Appointment } from "../src/Appointment";
+import { act } from "react-dom/test-utils";
+
+describe("Appointment", () => {
+  it("renders the customer first name", () => {
+    const customer = { firstName: "Ashley" };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement("div");
+    document.body.replaceChildren(container);
+
+    act(() => ReactDOM.createRoot(container).render(component));
+    expect(document.body.textContent).toContain("Ashley");
+  });
+
+  it("renders another customer first name", () => {
+    const customer = { firstName: "Jordan" };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement("div");
+    document.body.replaceChildren(container);
+
+    act(() => ReactDOM.createRoot(container).render(component));
+
+    expect(document.body.textContent).toContain("Jordan");
+  });
+});
